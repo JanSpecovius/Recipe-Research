1b7f8a9fc8b73497a70c558240ac00acb9d5330a
Christopher Caspari <caspari.christop.a21@student.dhbw-Karlsruhe.de>
2023-02-21 15:46:59 +0100

    LoadingDialog implementiert Random Recipes funktionieren

--- app/src/main/java/com/example/recipe_research/MainActivity.java
+++ app/src/main/java/com/example/recipe_research/MainActivity.java
@@ -11,6 +11,8 @@ import org.json.JSONObject;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +20,15 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recipe_research.Adapters.RandomRecipeAdapter;
+import com.example.recipe_research.Listeners.RandomRecipeResponseListener;
+import com.example.recipe_research.Models.RandomRecipeApiResponse;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -31,18 +37,47 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar dialog_loading;
+    LoadingDialog loadingDialog;
     RequestManager manager;
     RandomRecipeAdapter randomRecipeAdapter;
     RecyclerView recyclerView;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadingDialog = new LoadingDialog(MainActivity.this);
+        manager = new RequestManager(this);
+        manager.getRandomRecipes(randomRecipeResponseListener);
+
+        loadingDialog.showLoading();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadingDialog.disMiss();
+
+            }
+        }, 4000);
     }
 
+    private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
+        @Override
+        public void didFetch(RandomRecipeApiResponse response, String message) {
+            recyclerView = findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
+            randomRecipeAdapter = new RandomRecipeAdapter(MainActivity.this, response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
+        }
+
+        @Override
+        public void didError(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
 
