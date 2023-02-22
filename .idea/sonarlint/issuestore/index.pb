92f5ce637ea3f6c9f1adfc59555e93c5fc1ac42d
dankress <113473726+dankress@users.noreply.github.com>
2023-02-22 09:20:50 +0100

    add drop down test

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,18 @@
 
 P
  app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+o
+?app/src/main/java/com/example/recipe_research/MainActivity.java,5\8\58290558e8f11beab6994aa15f0200e8b9780d0b
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+j
+:app/src/main/java/com/example/recipe_research/Secrets.java,2\2\22c8bd9a4459ff486c4957fea90362dbb9a98701
+i
+9app/src/main/java/com/example/recipe_research/Search.java,0\7\07ffca86434eb54a23fe69617b46d9671fbd69c4
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+p
+@app/src/androidTest/java/com/example/recipe_research/UiTest.java,3\1\312f7b667d5fb2b7317e408b8cedd1c32a0120ce
--- app/src/androidTest/java/com/example/recipe_research/UiTest.java
+++ app/src/androidTest/java/com/example/recipe_research/UiTest.java
@@ -1,4 +1,5 @@
 package com.example.recipe_research;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -8,6 +9,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isNotClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -16,28 +18,51 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class UiTest {
-    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRuleMain
             = new ActivityScenarioRule<>(MainActivity.class);
 
+    @Rule
+    public ActivityScenarioRule<Search> activityScenarioRuleSearch
+            = new ActivityScenarioRule<>(Search.class);
 
     @Test
-    public void AgraphicDoesExist() {
+    public void graphicDoesExist() {
         onView(withId(R.id.lottieAnimationView)).check(matches(isCompletelyDisplayed()));
         onView(withId(R.id.lottieAnimationView)).check(matches(isNotClickable()));
+
     }
 
     @Test
-    public void BtoSearchActivity(){
+    public void switchActivity(){
         onView(withId(R.id.startButton)).check(matches(isCompletelyDisplayed()));
         onView(withId(R.id.startButton)).check(matches(isClickable()));
         onView(withId(R.id.startButton)).check(matches(withText("Start")));
         onView(withId(R.id.startButton)).perform(click());
-        onView(withId(R.id.veganSwitch)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.glutenFreeSwitch)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.veganSwitch)).perform(click()).check(matches(isClickable())).check(matches(withText("Vegan"))).check(matches(isCompletelyDisplayed()));
+
     }
 
+
+    @Test
+    public void searchActivity() {
+
+
+        onView(withId(R.id.veganSwitch)).check(matches(isCompletelyDisplayed()));
+        onView(withId(R.id.glutenFreeSwitch)).perform(click()).check(matches(isClickable())).check(matches(withText("Glutenfrei"))).check(matches(isCompletelyDisplayed()));
+        onView(withId(R.id.vegetarianSwitch)).perform(click()).check(matches(isClickable())).check(matches(withText("Vegetarisch"))).check(matches(isCompletelyDisplayed()));
+        onView(withId(R.id.veganSwitch)).perform(click()).check(matches(isClickable())).check(matches(withText("Vegan"))).check(matches(isCompletelyDisplayed()));
+
+        String[] testArgs = new String[]{"Main course","Side dish", "Dessert", "Appetizer", "Salad", "Bread", "Breakfast", "Soup", "Beverage", "Source", "Marinade", "Fingerfood", "Snack", "Drink"};
+
+        for (String testArg : testArgs) {
+            onView(withId(R.id.menuButton)).check(matches(isCompletelyDisplayed())).check(matches(isClickable())).perform(click());
+            onView(withText(testArg)).check(matches(isCompletelyDisplayed())).perform(click());
+            onView(withId(R.id.menuButton)).check(matches(withText(testArg)));
+        }
+    }
 }
